52e94fb8cc45fb0d2928d2cf42d268d2602e127c
promodAravinda95 <promodarvinda95@gmail.com>
2020-04-16 22:28:06 +0530

    doctor

--- appointment-api/src/main/resources/script/master-data.sql
+++ appointment-api/src/main/resources/script/master-data.sql
@@ -1,3 +1,6 @@
 USE sch_appointment;
 
-INSERT INTO appointment_status VALUES (1, 'Pending'), (2, 'Completed'), (3, 'Canceled');
+INSERT INTO appointment_status
+VALUES (1, 'Pending'),
+       (2, 'Completed'),
+       (3, 'Canceled');
--- payment-api/src/main/resources/script/master-data.sql
+++ payment-api/src/main/resources/script/master-data.sql
@@ -1,3 +1,5 @@
 USE sch_payment;
 
-INSERT INTO payment_status VALUES (1, 'Success'), (2, 'Refund');
+INSERT INTO payment_status
+VALUES (1, 'Success'),
+       (2, 'Refund');
