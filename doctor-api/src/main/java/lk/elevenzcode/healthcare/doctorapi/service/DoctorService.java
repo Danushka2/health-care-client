c23b059e9f4b340f26bebe694c702c3eb857d5dd
promodAravinda95 <promodarvinda95@gmail.com>
2020-04-19 13:08:50 +0530

    doctor authentication

--- doctor-api/src/main/java/lk/elevenzcode/healthcare/doctorapi/service/DoctorService.java
+++ doctor-api/src/main/java/lk/elevenzcode/healthcare/doctorapi/service/DoctorService.java
@@ -1,8 +1,11 @@
 package lk.elevenzcode.healthcare.doctorapi.service;
 
+import lk.elevenzcode.healthcare.commons.exception.ServiceException;
 import lk.elevenzcode.healthcare.commons.service.GenericService;
 import lk.elevenzcode.healthcare.doctorapi.domain.Doctor;
+import lk.elevenzcode.healthcare.doctorapi.web.dto.DoctorRegistration;
 
 
 public interface DoctorService extends GenericService<Doctor> {
+  int register(DoctorRegistration registerDto) throws ServiceException;
 }
--- doctor-api/src/main/java/lk/elevenzcode/healthcare/doctorapi/service/impl/DoctorServiceImpl.java
+++ doctor-api/src/main/java/lk/elevenzcode/healthcare/doctorapi/service/impl/DoctorServiceImpl.java
@@ -1,12 +1,19 @@
 package lk.elevenzcode.healthcare.doctorapi.service.impl;
 
+import lk.elevenzcode.healthcare.commons.enums.UserType;
 import lk.elevenzcode.healthcare.commons.exception.ServiceException;
 import lk.elevenzcode.healthcare.commons.service.impl.GenericServiceImpl;
+import lk.elevenzcode.healthcare.commons.util.Constant;
 import lk.elevenzcode.healthcare.doctorapi.domain.Doctor;
 import lk.elevenzcode.healthcare.doctorapi.repository.DoctorRepository;
 import lk.elevenzcode.healthcare.doctorapi.service.DoctorService;
+import lk.elevenzcode.healthcare.doctorapi.service.integration.AuthIntegrationService;
+import lk.elevenzcode.healthcare.doctorapi.service.integration.dto.UserRegDto;
+import lk.elevenzcode.healthcare.doctorapi.web.dto.DoctorRegistration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import javax.annotation.PostConstruct;
@@ -16,6 +23,9 @@ import javax.annotation.PostConstruct;
 public class DoctorServiceImpl extends GenericServiceImpl<Doctor> implements DoctorService {
   @Autowired
   private DoctorRepository doctorRepository;
+
+  @Autowired
+  private AuthIntegrationService authIntegrationService;
 
   @PostConstruct
   void init() {
@@ -62,6 +72,53 @@ public class DoctorServiceImpl extends GenericServiceImpl<Doctor> implements Doc
   public void delete(Integer id) throws ServiceException {
     doctorRepository.deleteById(id);
   }
+
+
+  //doctor register
+  @Override
+  @Transactional(value = Constant.TRANSACTION_MANAGER, propagation = Propagation.REQUIRED,
+      rollbackFor = ServiceException.class)
+  public int register(DoctorRegistration registerDto) throws ServiceException {
+    //create user account
+    final Integer userId = authIntegrationService.registerUser(new UserRegDto(registerDto
+        .getUsername(), registerDto.getPassword(), UserType.DOCTOR));
+
+    final Doctor doctor = new Doctor();
+
+
+    doctor.setSpecialization(registerDto.getSpecialization());
+    doctor.setName(registerDto.getName());
+    doctor.setTel(registerDto.getTel());
+    doctor.setEmail(registerDto.getEmail());
+    doctor.setStatus(registerDto.getStatus());
+    doctor.setUser_id(userId);
+    insert(doctor);
+
+
+    return doctor.getId();
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
