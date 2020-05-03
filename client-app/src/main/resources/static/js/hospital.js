7536c581a2471744b9dda8d81a6e8f2cd823c435
Danushka2 <dhanushkanuwan24@gmail.com>
2020-05-04 04:00:07 +0530

    created the hospital page with a modal to add hospital

---
+++ client-app/src/main/resources/static/js/hospital.js
@@ -0,0 +1,53 @@
+var $table = $('#hospital-table');
+var selections = [];
+
+$(function () {
+    $('#hospital-menu').addClass('active');
+    $('#hospital-menu').siblings().removeClass('active');
+    initTable();
+});
+
+function initTable() {
+    $table.bootstrapTable('destroy').bootstrapTable({
+        height: '900',
+        columns: [{
+            title: 'ID',
+            field: 'reference',
+            align: 'center',
+            valign: 'middle'
+        }, {
+            title: 'NAME',
+            field: 'appointmentInfo.appointmentDate',
+            align: 'center',
+            valign: 'middle'
+        }, {
+            title: 'EMAIL',
+            field: 'appointmentInfo.session.from',
+            align: 'center',
+            valign: 'middle'
+        }, {
+            title: 'ADDRESS',
+            field: 'appointmentInfo.patient.name',
+            align: 'center',
+            valign: 'middle'
+        }, {
+            title: 'TYPE',
+            field: 'fee',
+            align: 'center',
+            class: 'fee'
+        }, {
+            title: 'TELEPHONE',
+            field: 'paidOn',
+            align: 'center',
+            sortable: true
+        }],
+        classes: 'table',
+    });
+
+    $table.on('load-success.bs.table', function (data) {
+        $('.info').popover({
+            html: true,
+            placement: 'bottom'
+        });
+    });
+}
