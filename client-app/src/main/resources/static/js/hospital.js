d1a87791af7cd53c5a7319acbff48d8552bab473
Danushka2 <dhanushkanuwan24@gmail.com>
2020-05-05 00:37:56 +0530

    hospitals can be deleted

--- client-app/src/main/resources/static/js/hospital.js
+++ client-app/src/main/resources/static/js/hospital.js
@@ -1,3 +1,4 @@
+var HOSPITAL_DELETE_URL = '/hospitals/{id}';
 var $table = $('#hospital-table');
 var selections = [];
 
@@ -40,6 +41,11 @@ function initTable() {
             field: 'hospitalTell',
             align: 'center',
             sortable: true
+        }, {
+            title: COL_ACTIONS,
+            align: 'center',
+            events: window.actionEvents,
+            formatter: actionFormatter
         }],
         classes: 'table',
     });
@@ -52,6 +58,18 @@ function initTable() {
     });
 }
 
+function actionFormatter(value, row, index) {
+    var actions = '';
+    
+        actions += '<button class="btn btn-link link-primary edit" data-toggle="modal" data-target="#modifyPaymentModal" '
+            + 'data-backdrop="static" data-id="' + row.id + '" title="' + "Edit" + '">'
+            + '<i class="fad fa-edit"></i></button>'
+            + '<button class="btn btn-link link-danger refund" data-toggle="modal" data-target="#delete-form" '
+            + 'data-backdrop="static" data-toggle="tooltip" data-id="' + row.id + '" title="' + "Delete" + '">'
+            + '<i class="fas fa-trash-alt"></i></button>';
+    
+    return actions;
+}
 
 
 $('#create-form form').on('submit', function (e) {
@@ -66,3 +84,23 @@ $('#create-form form').on('submit', function (e) {
         });
     }
 });
+
+$('#delete-form').on('show.bs.modal', function (e) {
+    var $invoker = $(e.relatedTarget);
+    var id = $invoker.data('id');
+    $('#delete-form form').attr('action', HOSPITAL_DELETE_URL.replace('{id}', id));
+}).on('hidden.bs.modal', function (e) {
+    $('#delete-form form').attr('action', HOSPITAL_DELETE_URL);
+});
+
+$('#delete-form form').on('submit', function (e) {
+    e.preventDefault();
+    var $form = $(this);
+    ajaxFormJson($form, function (response) {
+        hideProgress();
+        $('#delete-form').modal('hide');
+        showNotification('success', 'Successfully Deleted!');
+        $table.bootstrapTable('refresh');
+    });
+});
+
