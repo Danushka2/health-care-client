20fa2c3a9918f367cd08871f4841c20e6de3d680
Danushka2 <dhanushkanuwan24@gmail.com>
2020-05-06 15:13:26 +0530

    added hospital dummy data

--- hospital-api/src/main/resources/script/dummy-data.sql
+++ hospital-api/src/main/resources/script/dummy-data.sql
@@ -1,3 +1,9 @@
 INSERT INTO sch_hospital.hospital (id, h_address, h_email, h_fax, h_name, h_status, h_tell, h_type, user_id) VALUES (1, '578 Elvitigala Mawatha, Colombo', 'info@lankahospitals.com', '0114511199', 'Base Hospitals', 'Available', '0115430000', 'private', 8);
+INSERT INTO sch_hospital.hospital (id, h_address, h_email, h_fax, h_name, h_status, h_tell, h_type, user_id) VALUES (2, '920 Perera Mawatha, Colombo 11', 'info@genaralhospitals.com', '0112233445', 'Genaral Hospitals', 'Available', '0112233445', 'government', 9);
+INSERT INTO sch_hospital.hospital (id, h_address, h_email, h_fax, h_name, h_status, h_tell, h_type, user_id) VALUES (3, '319/A Maithree Road, Panaruda', 'info@asirihospitals.com', '0111456829', 'Asiri Hospitals', 'Available', '0114511199', 'private', 10);
+INSERT INTO sch_hospital.hospital (id, h_address, h_email, h_fax, h_name, h_status, h_tell, h_type, user_id) VALUES (4, '13/B Elvitigala Mawatha, Ja-ela', 'info@delmonhospitals.com', '0123456789', 'Delmon Hospitals', 'Available', '0123456789', 'private', 11);
+INSERT INTO sch_hospital.hospital (id, h_address, h_email, h_fax, h_name, h_status, h_tell, h_type, user_id) VALUES (5, '20 kurupumulla Road, Kalutara', 'info@lankahospitals.com', '0114511199', 'Base Hospitals', 'Unavailable', '0115430000', 'government', 12);
+
+
 
 INSERT INTO sch_hospital.hospital_room (id, free, location, room_no, status, hospital_id) VALUES (1, 750.00, '5th Floor', '530A', 1, 1);
